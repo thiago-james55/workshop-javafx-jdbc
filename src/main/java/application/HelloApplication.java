47841532fd64432596b21070792108234e8b477c
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-06-29 11:13:22 -0300

    MainView design W.I.P

--- src/main/java/application/HelloApplication.java
+++ src/main/java/application/HelloApplication.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/gui/MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
