e92187e7e6a8c24e4e4ac4fcd62570f981d401c8
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-07-11 19:30:31 -0300

    Department obj injected in DepartmentForm

--- src/main/java/controller/DepartmentFormController.java
+++ src/main/java/controller/DepartmentFormController.java
@@ -5,12 +5,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 import util.Constraints;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DepartmentFormController implements Initializable {
+
+    private Department entity;
 
     @FXML
     private Label labelTitle;
@@ -29,6 +32,10 @@ public class DepartmentFormController implements Initializable {
 
     @FXML
     private Button buttonCancel;
+
+    public void setDepartment(Department entity) {
+        this.entity = entity;
+    }
 
     @FXML
     public void onButtonSaveAction() {
@@ -49,5 +56,13 @@ public class DepartmentFormController implements Initializable {
         Constraints.setTextFieldInteger(textFieldId);
         Constraints.setTextFieldMaxLength(textFieldName,30);
 
+    }
+
+    public void updateFormData() {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was null");
+        }
+        textFieldId.setText(String.valueOf(entity.getId()));
+        textFieldName.setText(entity.getName());
     }
 }
